a497c42c23f02e261c27decdfc81cb765070f90a
Capi Etheriel <barraponto@gmail.com>
2015-10-08 18:55:59 -0300

    exploratory look into data

---
+++ hackmar/spiders/acervo.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class AcervoSpider(scrapy.Spider):
+    name = "acervo"
+    allowed_domains = ["192.168.16.12"]
+    item_url_template = 'http://192.168.16.12/pergamum/biblioteca/index.php?rs=ajax_dados_acervo&rsargs[]={}'
+
+    def __init__(self, exploratory=None, *args, **kwargs):
+        super(AcervoSpider, self).__init__(*args, **kwargs)
+        self.exploratory = bool(exploratory)
+
+    def start_requests(self):
+        for number in range(1, 11111):
+            yield scrapy.Request(self.item_url_template.format(number))
+
+    def parse(self, response):
+        if self.exploratory:
+            for field in response.css('table strong::text').extract():
+                yield {'field': field.strip()}
+        else:
+            pass
