604ac3b28d1e52bdf8f3ed26d2d430c7ee757259
Capi Etheriel <barraponto@gmail.com>
2015-10-10 13:17:32 -0300

    fix selectors for the better, unescaped html

--- hackmar/spiders/acervo.py
+++ hackmar/spiders/acervo.py
@@ -5,7 +5,9 @@ import scrapy
 class AcervoSpider(scrapy.Spider):
     name = "acervo"
     allowed_domains = ["192.168.16.12"]
-    item_url_template = 'http://192.168.16.12/pergamum/biblioteca/index.php?rs=ajax_dados_acervo&rsargs[]={}'
+    item_url_template = ('http://192.168.16.12'
+                         '/pergamum/biblioteca/index.php'
+                         '?rs=ajax_dados_acervo&rsargs[]={}')
 
     def __init__(self, exploratory=None, *args, **kwargs):
         super(AcervoSpider, self).__init__(*args, **kwargs)
@@ -13,14 +15,29 @@ class AcervoSpider(scrapy.Spider):
 
     def start_requests(self):
         for number in range(1, 11111):
-            yield scrapy.Request(self.item_url_template.format(number))
+            yield scrapy.Request(
+                self.item_url_template.format(number))
 
     def parse(self, response):
-        response = response.replace(body=response.body.decode('string_escape'))
+        response = response.replace(
+            body=response.body.decode('string_escape'))
+
+        # if response.xpath('//div[not(strong)]/a'):
+            # # i think if there's a link in there.
+            # # let's look at it manually
+            # self.logger.warning(
+                # 'Link found in {}'.format(response.url))
+
         if self.exploratory:
+            # just get the field names, for exploratory looks.
             for field in response.css('table strong::text').extract():
                 yield {'field': field.strip()}
+
         else:
-            fields = response.css('table strong::text').extract()
-            values = response.xpath('//div[not(strong)]/text()').extract()[1:len(fields)+1]
+            # get the whole data.
+            fields = response.css(
+                '.layer_meio strong::text').extract()
+            values = (response.css('.layer_meio td:nth-child(2)')
+                      .xpath('string(.)')
+                      .extract())
             yield dict(zip(fields, values))
