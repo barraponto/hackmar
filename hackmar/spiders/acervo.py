c4bce259d9109f610b77b0c337623d98aba5146c
Capi Etheriel <barraponto@gmail.com>
2015-10-10 15:33:12 -0300

    cleanup spider code, add pergamus id

--- hackmar/spiders/acervo.py
+++ hackmar/spiders/acervo.py
@@ -9,10 +9,6 @@ class AcervoSpider(scrapy.Spider):
                          '/pergamum/biblioteca/index.php'
                          '?rs=ajax_dados_acervo&rsargs[]={}')
 
-    def __init__(self, exploratory=None, *args, **kwargs):
-        super(AcervoSpider, self).__init__(*args, **kwargs)
-        self.exploratory = bool(exploratory)
-
     def start_requests(self):
         for number in range(1, 11111):
             yield scrapy.Request(
@@ -22,22 +18,10 @@ class AcervoSpider(scrapy.Spider):
         response = response.replace(
             body=response.body.decode('string_escape'))
 
-        # if response.xpath('//div[not(strong)]/a'):
-            # # i think if there's a link in there.
-            # # let's look at it manually
-            # self.logger.warning(
-                # 'Link found in {}'.format(response.url))
-
-        if self.exploratory:
-            # just get the field names, for exploratory looks.
-            for field in response.css('table strong::text').extract():
-                yield {'field': field.strip()}
-
-        else:
-            # get the whole data.
-            fields = response.css(
-                '.layer_meio strong::text').extract()
-            values = (response.css('.layer_meio td:nth-child(2)')
-                      .xpath('string(.)')
-                      .extract())
-            yield dict(zip(fields, values))
+        fields = response.css(
+            '.layer_meio strong::text').extract()
+        values = (response.css('.layer_meio td:nth-child(2)')
+                  .xpath('string(.)')
+                  .extract())
+        values['pergamus_id'] = response.url.split('=')[-1]
+        yield dict(zip(fields, values))
